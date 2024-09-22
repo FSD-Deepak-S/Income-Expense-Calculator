90b2c457e3298d32d56153334f00a9161cfd6b1e
FSD-Deepak-S <devold94@gmail.com>
2024-09-23 00:50:05 +0530

    Condition added for adding expense

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const tincome = document.getElementById("tincome");
 const tbalance = document.getElementById("tbalance");
 const texpense = document.getElementById("texpense");
 const closebtn = document.getElementById("close");
+const add_expense_btn = document.getElementById("add_expense");
 let totalIncome;
 let totalExpense;
 let totalBalance;
@@ -31,11 +32,15 @@ const totalExp = () => {
     .map((e) => {
       totalExpense += parseInt(e.amount);
     });
-  totalBalance =   totalIncome - totalExpense
-  tincome.innerHTML = `₹${totalIncome.toLocaleString('en-IN')}`;
-  texpense.innerHTML = `₹${totalExpense.toLocaleString('en-IN')}`;
-  tbalance.innerHTML = `₹${totalBalance.toLocaleString('en-IN')}`;
-  
+  totalBalance = totalIncome - totalExpense;
+  tincome.innerHTML = `₹${totalIncome.toLocaleString("en-IN")}`;
+  texpense.innerHTML = `₹${totalExpense.toLocaleString("en-IN")}`;
+  tbalance.innerHTML = `₹${totalBalance.toLocaleString("en-IN")}`;
+  if (totalIncome === 0 || totalBalance === 0) {
+    add_expense_btn.classList.add("hidden");
+  } else {
+    add_expense_btn.classList.remove("hidden");
+  }
 };
 
 // form to open when click Add income/expense & edit income/expense
@@ -49,12 +54,11 @@ const IEform = (e) => {
   document.getElementById("form-title").innerText = `Enter ${e.value} details`;
 };
 
-
-// transaction filter all , income and expense 
+// transaction filter all , income and expense
 
 const filterTrans = (e) => {
   if (e.value === "Income") {
-    fdata = data.filter((ele,y) => ele.type === "Income");
+    fdata = data.filter((ele, y) => ele.type === "Income");
   } else if (e.value === "Expense") {
     fdata = data.filter((ele) => ele.type === "Expense");
   } else {
@@ -63,7 +67,7 @@ const filterTrans = (e) => {
   createTransaction(e.value);
 };
 
-// form submit btn  and close btn 
+// form submit btn  and close btn
 
 form.addEventListener("submit", (e) => {
   if (e.submitter.id === "submit") {
@@ -78,56 +82,72 @@ form.addEventListener("submit", (e) => {
   }
 });
 
-// form validation 
+// form validation
 
 const formValidation = () => {
   if (description.value === "" || amount.value === "") {
     descError.innerHTML = "Input Fields Cannot be Empty";
-    descError.style.color = 'red'
+    descError.style.color = "red";
   } else {
-    descError.innerHTML = "Data Saved Successfully";
-    descError.classList.add('text-green-500','bg-white','p-2')
+    if (type.value === "Expense") {
+      if (totalIncome < totalExpense + Number(amount.value)) {
+        descError.innerHTML = "Expense is Higher than Income";
+        descError.classList.add("text-red-500", "bg-white", "p-2");
+        setTimeout(() => {
+          descError.innerHTML = "";
+          descError.classList.remove("text-red-500", "bg-white", "p-2");
+        }, 3000);
+      } else {
+        descError.innerHTML = `${type.value} Saved Successfully`;
+        descError.classList.add("text-green-500", "bg-white", "p-2");
 
-    setTimeout(() => {
+        setTimeout(() => {
+          getData();
+        }, 1000);
+      }
+    } else {
+      descError.innerHTML = `${type.value} Saved Successfully`;
+      descError.classList.add("text-green-500", "bg-white", "p-2");
+
+      setTimeout(() => {
         getData();
-    }, 1000);
-
-    
+      }, 1000);
+    }
   }
 };
 
-// After form submit datas store to array and localStorage 
+// After form submit datas store to array and localStorage
 
 const getData = () => {
   data.push({
     type: type.value,
     description: description.value,
     amount: amount.value,
-
   });
   fdata = data.filter((ele) => ele);
   localStorage.setItem("data", JSON.stringify(data));
   form.classList.add("hidden");
   form.reset();
   descError.innerHTML = "";
-  descError.classList.remove('text-green-500','bg-white','p-2')
-//   alert(`${type.value} Added`);
+  descError.classList.remove("text-green-500", "bg-white", "p-2");
+  //   alert(`${type.value} Added`);
   dashboard.classList.remove("hidden");
   fdata = data.filter((ele) => ele);
   createTransaction("all");
 };
 
-
-// Display the localStorage data to transaction details 
+// Display the localStorage data to transaction details
 
 const createTransaction = (tfilter) => {
   transaction.innerHTML = "";
-  if (tfilter === 'all') {
+  if (tfilter === "all") {
     data.map((ele, y) => {
       transaction.innerHTML += `<tr id=${y} class="border h-10">
                   <td class="">${ele.type}</td>
                   <td class="">${ele.description}</td>
-                  <td class="">₹ ${parseInt(ele.amount).toLocaleString('en-IN')}</td>
+                  <td class="">₹ ${parseInt(ele.amount).toLocaleString(
+                    "en-IN"
+                  )}</td>
                   <td class="">
                     <button  type="button" class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300" onclick="edittrans(this)">
                       edit
@@ -138,13 +158,15 @@ const createTransaction = (tfilter) => {
                   </td>
                 </tr>`;
     });
-  }else{
-    data.filter((ele,y)=>{
+  } else {
+    data.filter((ele, y) => {
       if (ele.type === tfilter) {
         transaction.innerHTML += `<tr id=${y} class="border">
                   <td class="">${ele.type}</td>
                   <td class="">${ele.description}</td>
-                  <td class="">₹ ${parseInt(ele.amount).toLocaleString('en-IN')}</td>
+                  <td class="">₹ ${parseInt(ele.amount).toLocaleString(
+                    "en-IN"
+                  )}</td>
                   <td class="">
                     <button type="button" class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300" onclick="edittrans(this)">
                       edit
@@ -154,10 +176,8 @@ const createTransaction = (tfilter) => {
                     </button>
                   </td>
                 </tr>`;
-        
       }
-      
-    })
+    });
   }
 
   totalExp();
@@ -169,13 +189,11 @@ const createTransaction = (tfilter) => {
   createTransaction("all");
 })();
 
-
-
 // Delete the transaction
 
 const deletetrans = (ele) => {
   ele.parentElement.parentElement.remove();
-  data.splice(ele.parentElement.parentElement.id,1)
+  data.splice(ele.parentElement.parentElement.id, 1);
   localStorage.setItem("data", JSON.stringify(data));
   totalExp();
   createTransaction("all");
@@ -189,10 +207,12 @@ const edittrans = (e) => {
   dashboard.classList.add("hidden");
   type.value = result.children[0].innerHTML;
   description.value = result.children[1].innerHTML;
-  amount.value = Number(result.children[2].innerHTML.split(" ")[1].replace(/[^0-9.-]+/g,""));
+  amount.value = Number(
+    result.children[2].innerHTML.split(" ")[1].replace(/[^0-9.-]+/g, "")
+  );
 
   // console.log( );
-  
+
   document.getElementById(
     "form-title"
   ).innerText = `Edit ${type.value} details`;
